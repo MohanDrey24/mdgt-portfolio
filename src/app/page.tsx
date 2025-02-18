428808caa899e4383d59556a8768417c7d532da7
MohanDrey24 <mohantampon@gmail.com>
2025-02-18 17:03:21 +0800

    remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Hero from "./components/Hero";
 export default function Home() {
   return (
     <div className="relative z-0 bg-primary">
-      {/* <div className="bg-[url('/assets/herobg.png')] bg-cover bg-no-repeat bg-center"> */}
       <div className="bg-amber-400">
         <Hero />
       </div>
