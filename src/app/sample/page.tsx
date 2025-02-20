eca45ea4ec79e91482e2e1987e60fcff0894dc31
MohanDrey24 <mohantampon@gmail.com>
2025-02-20 17:47:31 +0800

    update sample page

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,15 +1,17 @@
-import TypewriterEffect from "../components/TypewriterEffect";
+import ParticlesContainer from "../components/ParticlesContainer";
+// import TypewriterEffect from "../components/TypewriterEffect";
 
 const Sample = () => {
-  const examples = [
-    "I'm a full-stack developer",
-    "Wtf is happening with this shit",
-    "I create and fix bugs",
-  ];
+  // const examples = [
+  //   "I'm a full-stack developer",
+  //   "Wtf is happening with this shit",
+  //   "I create and fix bugs",
+  // ];
 
   return (
     <div className="flex h-screen items-center justify-center flex-col">
-      <TypewriterEffect text={examples} />
+      {/* <TypewriterEffect text={examples} /> */}
+      <ParticlesContainer />
     </div>
   );
 };
