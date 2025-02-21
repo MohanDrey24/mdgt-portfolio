1c520ebbcb32b26fdd62a9b144106c8881f9ebaf
MohanDrey24 <mohantampon@gmail.com>
2025-02-21 17:46:00 +0800

    update root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,42 @@
 import HorizontalScroll from "./components/HorizontalScroll";
 import Hero from "./components/Hero";
 import ParticlesContainer from "./components/ParticlesContainer";
+import ScrollIndicator from "./components/ScrollIndicator";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  const onScroll = () => {
+    if (containerRef.current) {
+      const { scrollTop, scrollHeight, clientHeight } = containerRef.current;
+      const hasReachedBottom =
+        Math.abs(scrollTop + clientHeight - scrollHeight) < 1;
+
+      if (hasReachedBottom) {
+        console.log("THE BOTTOM HAS BEEN REACHED");
+      }
+    }
+  };
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    if (container) {
+      container.addEventListener("scroll", onScroll);
+
+      return () => {
+        container.removeEventListener("scroll", onScroll);
+      };
+    }
+  }, []);
+
   return (
-    <div className="bg-amber-600 flex flex-col">
+    <main ref={containerRef} className="bg-amber-600 flex flex-col">
       <ParticlesContainer />
       <Hero />
       <HorizontalScroll />
-    </div>
+      <ScrollIndicator />
+    </main>
   );
 }
