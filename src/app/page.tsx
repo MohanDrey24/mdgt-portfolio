13614fb1c34621bb1a856fa5180ac1ee200e7751
Mohan Drey <mohantampon@gmail.com>
2025-02-23 21:17:48 +0800

    update root page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,17 @@ import HorizontalScroll from "./components/HorizontalScroll";
 import Hero from "./components/Hero";
 import ParticlesContainer from "./components/ParticlesContainer";
 import ScrollIndicator from "./components/ScrollIndicator";
+import { AuroraEffect } from "./components/AuroraEffect";
 
 export default function Home() {
+  const heroText = [
+    "I am a full-stack web developer who makes web applications.",
+  ];
   return (
-    <main className="flex flex-col">
-      <ParticlesContainer />
-      <Hero />
+    <main className="flex flex-col bg-[#E7EFF2FF]">
+      {/* <ParticlesContainer /> */}
+      <AuroraEffect />
+      <Hero heroText={heroText} isComputerVisible={true} />
       <HorizontalScroll />
       <ScrollIndicator className="bottom-4 left-1/2" />
     </main>
