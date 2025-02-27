b99a19602eabea8aa61e6d80ecdffb5d929fb82c
Mohan Drey <mohantampon@gmail.com>
2025-02-27 23:20:30 +0800

    add lenis smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,28 @@
 
 import HorizontalScroll from "./components/HorizontalScroll";
 import Hero from "./components/Hero";
-import ParticlesContainer from "./components/ParticlesContainer";
+// import ParticlesContainer from "./components/ParticlesContainer";
 import ScrollIndicator from "./components/ScrollIndicator";
 import { AuroraEffect } from "./components/AuroraEffect";
+import { useEffect } from "react";
+import Lenis from "lenis";
 
 export default function Home() {
   const heroText = [
     "I am a full-stack web developer who makes web applications.",
   ];
+
+  useEffect(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+
+    // Listen for the scroll event and log the event data
+    lenis.on("scroll", (e: Lenis) => {
+      console.log(e);
+    });
+  }, []);
+
   return (
     <main className="flex flex-col bg-[#E7EFF2FF]">
       {/* <ParticlesContainer /> */}
