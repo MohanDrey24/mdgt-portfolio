851a1e74411d613a04e6e6dcd2cb15efcd395907
Mohan Drey <mohantampon@gmail.com>
2025-03-01 23:23:09 +0800

    update lenis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,40 @@ import Hero from "./components/Hero";
 // import ParticlesContainer from "./components/ParticlesContainer";
 import ScrollIndicator from "./components/ScrollIndicator";
 import { AuroraEffect } from "./components/AuroraEffect";
-import { useEffect } from "react";
-import Lenis from "lenis";
+import { useEffect, useRef } from "react";
+import { LenisRef } from "lenis/react";
+import { cancelFrame, frame } from "framer-motion";
+import { ReactLenis } from "lenis/react";
 
 export default function Home() {
   const heroText = [
     "I am a full-stack web developer who makes web applications.",
   ];
 
-  useEffect(() => {
-    const lenis = new Lenis({
-      autoRaf: true,
-    });
+  const lenisRef = useRef<LenisRef>(null);
 
-    lenis.on("scroll", (e: Lenis) => {
-      console.log(e);
-    });
+  useEffect(() => {
+    function update(data: { timestamp: number }) {
+      const time = data.timestamp;
+      lenisRef.current?.lenis?.raf(time);
+    }
+
+    frame.update(update, true);
+
+    return () => cancelFrame(update);
   }, []);
 
   return (
-    <main className="flex flex-col bg-[#E7EFF2FF]">
+    <ReactLenis
+      options={{ autoRaf: false, lerp: 0.05 }}
+      ref={lenisRef}
+      className="flex flex-col bg-[#E7EFF2FF]"
+    >
       {/* <ParticlesContainer /> */}
       <AuroraEffect />
       <Hero heroText={heroText} isComputerVisible={true} />
       <HorizontalScroll />
       <ScrollIndicator className="bottom-4 left-1/2" />
-    </main>
+    </ReactLenis>
   );
 }
