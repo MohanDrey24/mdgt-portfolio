3a7f50815335c099e62eb9c136a05ef3623b8e5b
Mohan Drey <mohantampon@gmail.com>
2025-03-07 17:48:43 +0800

    add lenis

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -2,13 +2,31 @@
 
 import HorizontalScroll from "../components/HorizontalScroll";
 import { NewHero } from "../components/NewHero";
+import { ReactLenis } from "lenis/react";
+import type { LenisRef } from "lenis/react";
+import { cancelFrame, frame } from "framer-motion";
+import { useEffect, useRef } from "react";
+import "lenis/dist/lenis.css";
 
 const Sample = () => {
+  const lenisRef = useRef<LenisRef>(null);
+
+  useEffect(() => {
+    function update(data: { timestamp: number }) {
+      const time = data.timestamp;
+      lenisRef.current?.lenis?.raf(time);
+    }
+
+    frame.update(update, true);
+
+    return () => cancelFrame(update);
+  }, []);
+
   return (
-    <main>
+    <ReactLenis options={{ autoRaf: false }} ref={lenisRef}>
       <NewHero />
       <HorizontalScroll />
-    </main>
+    </ReactLenis>
   );
 };
 
