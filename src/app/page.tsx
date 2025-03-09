abc337442565927551ceb71cb5e5145004e9d288
Mohan Drey <mohantampon@gmail.com>
2025-03-09 22:44:15 +0800

    update index page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,39 +4,18 @@ import HorizontalScroll from "./components/HorizontalScroll";
 import Hero from "./components/Hero";
 import ScrollIndicator from "./components/ScrollIndicator";
 import { AuroraEffect } from "./components/AuroraEffect";
-import { useEffect, useRef } from "react";
-import { LenisRef } from "lenis/react";
-import { cancelFrame, frame } from "framer-motion";
-import { ReactLenis } from "lenis/react";
 
 export default function Home() {
   const heroText = [
     "I am a full-stack web developer who makes web applications.",
   ];
 
-  const lenisRef = useRef<LenisRef>(null);
-
-  useEffect(() => {
-    function update(data: { timestamp: number }) {
-      const time = data.timestamp;
-      lenisRef.current?.lenis?.raf(time);
-    }
-
-    frame.update(update, true);
-
-    return () => cancelFrame(update);
-  }, []);
-
   return (
-    <ReactLenis
-      options={{ autoRaf: false, lerp: 0.05 }}
-      ref={lenisRef}
-      className="flex flex-col bg-[#E7EFF2FF]"
-    >
+    <main className="flex flex-col">
       <AuroraEffect />
       <Hero heroText={heroText} isComputerVisible={true} />
       <HorizontalScroll />
       <ScrollIndicator className="bottom-4 left-1/2" />
-    </ReactLenis>
+    </main>
   );
 }
