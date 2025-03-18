208a8059366635b3131834b96eae5a85e8f6a8a4
Mohan Drey <mohantampon@gmail.com>
2025-03-18 19:00:25 +0800

    add masked component in new hero

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import HorizontalScroll from "../components/HorizontalScroll";
+import { MaskedComponent } from "../components/MaskedComponent";
 import { NewHero } from "../components/NewHero";
 
 const Sample = () => {
   return (
     <div>
       <NewHero />
+      <MaskedComponent />
       <HorizontalScroll />
     </div>
   );
