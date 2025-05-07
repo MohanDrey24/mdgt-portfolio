bc1e91fc83d0bf5d43aeb76cc4839a458896d81d
Mohan Drey <mohantampon@gmail.com>
2025-05-07 19:00:46 +0800

    update page

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,16 +1,14 @@
-"use client";
+'use client';
 
-import { FolderScroll } from "../components/FolderScroll";
-// import HorizontalScroll from "../components/HorizontalScroll";
-import { MaskedComponent } from "../components/MaskedComponent";
-import { NewHero } from "../components/NewHero";
+import { FolderScroll } from '@/app/components/FolderScroll';
+import { MaskedComponent } from '@/app/components/MaskedComponent';
+import { NewHero } from '@/app/components/NewHero';
 
 const Sample = () => {
   return (
     <div>
       <NewHero />
       <MaskedComponent />
-      {/* <HorizontalScroll /> */}
       <FolderScroll />
     </div>
   );
