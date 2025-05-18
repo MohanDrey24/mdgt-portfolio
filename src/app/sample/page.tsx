cc7f9d31965ad77ab1e20feb6961329d16f100c4
Mohan Drey <mohantampon@gmail.com>
2025-05-18 19:25:20 +0800

    update sample page

--- src/app/sample/page.tsx
+++ src/app/sample/page.tsx
@@ -1,15 +1,15 @@
-'use client';
+"use client";
 
-import { FolderScroll } from '@/app/components/FolderScroll';
-import { MaskedComponent } from '@/app/components/MaskedComponent';
-import { NewHero } from '@/app/components/NewHero';
+import { MaskedComponent } from "@/app/components/MaskedComponent";
+import { NewHero } from "@/app/components/NewHero";
+import { StackingCardsEffect } from "../components/StackingCardsEffect";
 
 const Sample = () => {
   return (
     <div>
       <NewHero />
       <MaskedComponent />
-      <FolderScroll />
+      <StackingCardsEffect />
     </div>
   );
 };
