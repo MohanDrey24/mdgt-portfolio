dbaacd31496e55a3337e43c1f9cfa87fcc44e191
Mohan Drey <mohantampon@gmail.com>
2025-05-20 18:50:03 +0800

    update stacking card effect padding

--- src/app/components/StackingCardsEffect.tsx
+++ src/app/components/StackingCardsEffect.tsx
@@ -3,7 +3,7 @@ import { projects } from "../utils/data";
 
 export const StackingCardsEffect = () => {
   return (
-    <main className="bg-black opacity-[0.85] pt-[50vh] pb-[50vh]">
+    <main className="bg-black opacity-[0.85] pt-[15vh] pb-[15vh]">
       {projects.map((proj, index) => {
         return <Card key={index} {...proj} index={index} />;
       })}
